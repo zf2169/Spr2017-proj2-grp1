16e44e5fcbc5d1889b21782f4bf08ae17baa406b
seanreddy <sean.patrick.reddy@gmail.com>
2017-02-20 21:16:57 -0500

    Modified code to generate heat maps based on shapefile regions. Added heat map overlay.

--- doc/createHeatMap.R
+++ doc/createHeatMap.R
@@ -1,24 +1,49 @@
 createHeatMap <- function(neighborhood="empty", borough, data, mapColor="blue"){
   
-  nbh <- read.csv("../data/neighborhoods.csv", header=TRUE)
-  centroidLocations <- read.csv("../output/neighborhood_centroid.csv", header=TRUE)
-  
-  listOfNeigh <- nbh[,eval(borough)]
-  idx <- which(listOfNeigh == neighborhood)
-  
-  listOfLats <- centroidLocations[,eval(paste0(borough, ".lat"))]
-  listOfLons <- centroidLocations[,eval(paste0(borough, ".lon"))]
-  centroidLAT <- listOfLats[idx]
-  centroidLON <- listOfLons[idx]
-  
-  if (neighborhood == "empty") {
+  if (neighborhood == "empty"){
+    centroids <- read.csv("../output/borough_centroids.csv", header=TRUE)
+    centroidLAT <- centroids$LAT[centroids$borough==borough]
+    centroidLON <- centroids$LON[centroids$borough==borough]
     initMap <- get_map(location = c(lon = centroidLON, lat = centroidLAT), zoom = 11)
+    heatmap <- ggmap(initMap, extent = "device") + stat_density2d(data = data[(data$borough == borough),], aes(x = LON, y = LAT, fill = ..level.., alpha = ..level..), size = 0.01, bins = 16, geom = "polygon") + scale_fill_gradient(low = mapColor, high = paste0(mapColor, "4")) + scale_alpha(range = c(0, 0.5), guide = FALSE)
   } else {
+    centroids <- read.csv("../output/shapefile_neighborhood_centroids.csv", header=TRUE)
+    centroidLAT <- centroids$LAT[centroids$neighborhood==neighborhood]
+    centroidLON <- centroids$LON[centroids$neighborhood==neighborhood]
     initMap <- get_map(location = c(lon = centroidLON, lat = centroidLAT), zoom = 15)
+    heatmap <- ggmap(initMap, extent = "device") + stat_density2d(data = data[(data$neighborhood == neighborhood),], aes(x = LON, y = LAT, fill = ..level.., alpha = ..level..), size = 0.01, bins = 16, geom = "polygon") + scale_fill_gradient(low = mapColor, high = paste0(mapColor, "4")) + scale_alpha(range = c(0, 0.5), guide = FALSE)
   }
   
+  return(heatmap)
+}
+
+createComboHeatMap <- function(neighborhood="None", borough, wifiData, taxiData, wifiColor="blue", taxiColor="red"){
   
-  heatmap <- ggmap(initMap, extent = "device") + stat_density2d(data = data[(data$neighborhood == neighborhood) & (data$borough == borough),], aes(x = LON, y = LAT, fill = ..level.., alpha = ..level..), size = 0.01, bins = 16, geom = "polygon") + scale_fill_gradient(low = mapColor, high = paste0(mapColor, "4")) + scale_alpha(range = c(0, 0.5), guide = FALSE)
+  if (neighborhood == "None"){
+    centroids <- read.csv("../output/borough_centroids.csv", header=TRUE)
+    centroidLAT <- centroids$LAT[centroids$borough==borough]
+    centroidLON <- centroids$LON[centroids$borough==borough]
+    initMap <- get_map(location = c(lon = centroidLON, lat = centroidLAT), zoom = 12)
+    fullData <- rbind(data.frame(taxiData[(taxiData$borough == borough),], group="Foot Traffic"), data.frame(wifiData[(wifiData$borough == borough),], group="WiFi Coverage"))
+    heatmap <- ggmap(initMap, extent = "device") +
+      stat_density2d(data = fullData,
+                     aes(x = LON, y = LAT, fill = group, alpha = ..level..),
+                     size = 0.01, bins = 16, geom = "polygon") + 
+      scale_fill_manual(values=c("Foot Traffic"=taxiColor, "WiFi Coverage"=wifiColor)) +
+      scale_alpha(range = c(0, 0.5))
+  } else {
+    centroids <- read.csv("../output/shapefile_neighborhood_centroids.csv", header=TRUE)
+    centroidLAT <- centroids$LAT[centroids$neighborhood==neighborhood]
+    centroidLON <- centroids$LON[centroids$neighborhood==neighborhood]
+    initMap <- get_map(location = c(lon = centroidLON, lat = centroidLAT), zoom = 15)
+    fullData <- rbind(data.frame(taxiData[(taxiData$neighborhood == neighborhood),], group="Foot Traffic"), data.frame(wifiData[(wifiData$neighborhood == neighborhood),], group="WiFi Coverage"))
+    heatmap <- ggmap(initMap, extent = "device") +
+      stat_density2d(data = fullData,
+                     aes(x = LON, y = LAT, fill = group, alpha = ..level..),
+                     size = 0.01, bins = 16, geom = "polygon") + 
+      scale_fill_manual(values=c("Foot Traffic"=taxiColor, "WiFi Coverage"=wifiColor)) +
+      scale_alpha(range = c(0, 0.3))
+  }
   
   return(heatmap)
 }
