716fec390b5fcae76eea49bd64ad0c789012aaaa
zf2169 <zf2169@columbia.edu>
2017-02-21 21:22:16 -0500

    #new

--- app/server.R
+++ app/server.R
@@ -5,22 +5,13 @@ library(dplyr)
 
 data_sets <- read.csv("../output/neighborhood_centroids_latlong.csv", header = T)
 getsubselection<- function(borough) {
-  if (borough==0) borough <- "Manhattan"
-  if (borough==1) borough <- "Bronx"
-  if (borough==2) borough <- "Queens"
-  if (borough==3) borough <- "Brooklyn"
-  if (borough==4) borough <- "Staten_Island"
   selection<- data_sets%>%
               filter(Borough==borough)%>%
               select(NeighborhoodName)
   l<- vector("list", nrow(selection))
-  n<-c()
   for(i in 1:nrow(selection)) {
-    c<- as.character(selection$NeighborhoodName[i])
-    n<- cbind(n,c)
-    l[i]<-i
+    l[i] <- as.character(selection$NeighborhoodName[i])
   }
-  names(l)<- n
   return(l)
 }
 
@@ -34,7 +25,7 @@ shinyServer(function(input, output) {
     
     # Create the selectbox
     selectInput("neighborhoodname", 
-                label = h5("Choose the neighborhood"), 
+                label = "Choose the neighborhood", 
                 choices  = dat,
                 selected = 1)
   })
--- app/ui.r
+++ app/ui.r
@@ -4,19 +4,19 @@ library(shiny)
 shinyUI(fluidPage(tabsetPanel(
 
   # page 1- map plotout page
-  tabPanel("layout",
+  tabPanel("Layout",
     sidebarLayout(
-      sidebarPanel(
-        selectInput("borough", label = h5("Choose a Borough"), 
-                           choices = list("Manhattan"=0,
-                                          "Bronx"=1, 
-                                          "Queens"=2,
-                                          "Brooklyn"=3,
-                                          "Staten Island"=4
+        sidebarPanel(
+        # selectbox to choose the borough
+        selectInput("borough", label = "Choose a Borough", 
+                           choices = list("Manhattan", "Bronx", 
+                                          "Queens", "Brooklyn",
+                                          "Staten Island"="Staten_Island"
                                           ),
                            selected = 0),
+        # selectbox to choose the neighborhood based on the borough
         uiOutput("neighborhood"),
-        selectInput("time", label=h5("Choose Time"),
+        selectInput("time", label="Choose Time",
                     choices = list("111"=1,
                                    "222"=2,
                                    "333"=3),
@@ -28,10 +28,25 @@ shinyUI(fluidPage(tabsetPanel(
       # the main panel of page 1
       mainPanel(
         # main title
-        h1("Wifi Spots/Heat map throughout New York"),
+        h1("Wifi Spots/Heat map throughout New York City"),
         plotOutput("map")
       ))),
   
-  tabPanel("Result","xxx")
+  tabPanel("Conclusion",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("result", label = "Places to be explored",
+                        choices  = list("Place 1"=1,
+                                       "Place 2"=2,
+                                       "Place 3"=3),
+                        selected = 1)
+        ),
+        mainPanel(
+          h1("Conclusion"),
+          textOutput("text"),
+          plotOutput("placemap")
+        )
+       )
+    )
 )))
 
